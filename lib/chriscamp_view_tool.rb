c94f0eb94a844c024fef76447ea286b98794987d
Chris <codemedarkone@gmail.com>
2017-03-08 12:47:31 -0700

    Dev tool initial commit

---
+++ lib/chriscamp_view_tool.rb
@@ -0,0 +1,6 @@
+require "chriscamp_view_tool/version"
+require "chriscamp_view_tool/rendere"
+
+module ChriscampViewTool
+  # Your code goes here...
+end
---
+++ lib/chriscamp_view_tool/version.rb
@@ -0,0 +1,3 @@
+module ChriscampViewTool
+  VERSION = "0.1.0"
+end
